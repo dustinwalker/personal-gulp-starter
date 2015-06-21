e3e008275ac1b93d80bf5e9e4e6087f239da8bdc
Dustin Walker <me@dustinwalker.com>
2015-06-20 19:41:34 -0500

    find unused CSS

--- gulpfile.js/tasks/test.js
+++ gulpfile.js/tasks/test.js
@@ -2,17 +2,35 @@ var gulp            = require('gulp'),
     handleErrors    = require('../lib/handleErrors'),
     config          = require('../config/test'),
     uncss           = require('gulp-uncss'),
+    cssbeautify     = require('gulp-cssbeautify'),
+    notify          = require('gulp-notify'),
     csslint         = require('gulp-csslint');
 
 gulp.task('test', function() {
   gulp.src(config.cssSrc)
+    .pipe(notify({
+        message: "Checking CSS against for potential issues:" + config.cssLintRules
+    }))
     .pipe(csslint(config.cssLintRules))
     .pipe(csslint.reporter())
+    .pipe(cssbeautify())
+    .pipe(gulp.dest('./test-results/before'))
+    .pipe(notify({
+        message: "-----------------------------------"
+    }))
+    .pipe(notify({
+        message: "Checking:" + config.urls + " for unused CSS"
+    }))
     .pipe(uncss({
         html: config.urls,
         options: {
             report: true
         }
     }))
-    .pipe(gulp.dest('./test-results'));
+    .pipe(cssbeautify())
+    .pipe(notify({
+        message: "Saved CSS files to /test-results/ to compare"
+    }))
+    .pipe(gulp.dest('./test-results/after'));
+
 });
